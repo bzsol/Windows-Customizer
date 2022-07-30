695070b6dd8dd4dfbd6a31c08a8f7fe249e63174
bzsol <bereczzsolt2000@gmail.com>
2022-07-30 16:44:06 +0200

    Updated script file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Automate-Windows-Install
+
+#### 1. Introduction:
+> This PowerShell code can automatize and make a Windows install easier for me.
+
+#### 2. How to use the program:
+- Download or clone the git repository
+- Once you extracted the .zip or opened the repository location open the PowerShell (Administrator mode recommended)
+- Use <code>Set-ExecutionPolicy Unrestricted -Force</code>
+- On the prompt, change to the directory where you extracted the <code>.ps1</code> file
--- automate.ps1
+++ automate.ps1
@@ -1,4 +1,4 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# Set-ExecutionPolicy Unrestricted -Force
 
 $ischangepcname = Read-Host "PC name change? (y/n)"
 if($ischangepcname -eq "y"){
@@ -13,29 +13,54 @@ if($isdnschange -eq "y"){
  Write-Host "DNS change has been done!"
 }
 
-$isprogramming = Read-Host "Is it for programming? (y/n)"
-# Choco installer
-if ( $isprogramming -eq "y")
-{
-choco install git -y
-choco install googlechrome -y
-choco install firefox -y
-choco install notepadplusplus -y
-choco install 7zip -y
-choco install teamviewer -y
-choco install putty -y
-choco install vscode -y
-choco install k-litecodecpackfull -y
-choco install winscp.install -y
-choco install qbittorrent -y
-choco install irfanview -y
-choco install microsoft-windows-terminal -y
-choco install python3 -y
-choco install discord -y
-choco install teamviewer -y
+Write-Host "Show hidden file extensions and hidden files as well!"
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name Hidden -Value 1
+Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name HideFileExt -Value 0
+
+Write-Host "Installing Chocolatey for installing the programs that needed for the user..."
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+$isforprogramming = Read-Host "Is it for programming? (y/n)"
+
+if($isforprogramming -eq "y"){
+	choco install googlechrome -y
+	choco install firefox -y
+	choco install 7zip -y
+	choco install vscode -y
+	choco install git -y
+	choco install steam-client -y
+	choco install discord -y
+	choco install k-litecodecpackfull -y
+	choco install postman -y
+	choco install visualstudio2022community -y
+	choco install qbittorrent -y
+	choco install irfanview -y
+	choco install teamviewer -y
+	choco install obs-studio -y
+	choco install sumatrapdf -y
+	choco install nodejs -y
+	choco install microsoft-windows-terminal -y
+	choco install arduino -y
+	choco install winscp -y
+	choco install jdk11 -y
+	choco install teamspeak -y
+	choco install revo-uninstaller -y
 }
 else{
 
 }
+if([System.Environment]::OSVersion.Version.Major -eq 10){
+	Write-Host "Windows 10 Debloater"
+	Invoke-WebRequest -useb https://git.io/debloat | Invoke-Expression
+}
+elseif([System.Environment]::OSVersion.Version.Major -eq 11){
+    Invoke-Expression "& {$(Invoke-RestMethod get.scoop.sh)} -RunAsAdmin"
+	scoop bucket add extras
+	scoop install thisiswin11
+}
+else{
+	Write-Error "You are using an old Windows system!"
+}
+
 
 
